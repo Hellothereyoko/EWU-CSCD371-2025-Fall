a9565050c080f3739ac7c41b64bdb641627b8011
Nico Duvekot <nduvekot7@gmail.com>
2025-10-03 15:25:47 -0700

    add: Error mixin

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -1,6 +1,12 @@
-﻿namespace Logger;
+﻿using System;
+
+namespace Logger;
 
 public static class BaseLoggerMixins
 {
-
+    public static void Error(this BaseLogger? logger, string message, params object[] args)
+    {
+        ArgumentNullException.ThrowIfNull(logger);
+        logger.Log(LogLevel.Error, string.Format(message, args));
+    }
 }
