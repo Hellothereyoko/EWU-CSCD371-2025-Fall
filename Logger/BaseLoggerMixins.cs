2e295f6659796c7e203234b46360573974664871
Nico Duvekot <nduvekot7@gmail.com>
2025-10-03 16:09:05 -0700

    refactor: EnsureLogger
    Ensure logger now returns the logger
    
    BaseLogger object is a reference object
    passed in: reference to a BaseLogger object, if that is null, throw error, else, return the logger
    
    does not copy, just ensures the reference does in fact point to an existing BaseLogger object

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -6,12 +6,12 @@ public static class BaseLoggerMixins
 {
     public static void Error(this BaseLogger? logger, string message, params object[] args)
     {
-        ArgumentNullException.ThrowIfNull(logger);
-        logger.Log(LogLevel.Error, string.Format(message, args));
+        EnsureLogger(logger).Log(LogLevel.Error, string.Format(message, args));
     }
 
-    public static void EnsureLogger(BaseLogger? logger)
+    public static BaseLogger EnsureLogger(BaseLogger? logger)
     {
         ArgumentNullException.ThrowIfNull(logger);
+        return logger;
     }
 }
