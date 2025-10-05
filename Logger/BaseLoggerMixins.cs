93116f9c7efe41d64809be3911def1c27e491cd4
BillMillerCoding <billmillercoding@outlook.com>
2025-10-04 18:51:06 -0700

    Rough Draft for Logger class heirarchy

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -1,6 +1,43 @@
-﻿namespace Logger;
+﻿using System;
+
+namespace Logger;
 
 public static class BaseLoggerMixins
 {
-
+    public static void Error(this BaseLogger logger, string message, params object[] args)
+    {
+        if (logger == null)
+        {
+            throw new ArgumentNullException(nameof(logger));
+        }
+        string formattedMessage = string.Format(message, args);
+        logger.Log(LogLevel.Error, formattedMessage);
+    }
+    public static void Warning(this BaseLogger logger, string message, params object[] args)
+    {
+        if (logger == null)
+        {
+            throw new ArgumentNullException(nameof(logger));
+        }
+        string formattedMessage = string.Format(message, args);
+        logger.Log(LogLevel.Warning, formattedMessage);
+    }
+    public static void Information(this BaseLogger logger, string message, params object[] args)
+    {
+        if (logger == null)
+        {
+            throw new ArgumentNullException(nameof(logger));
+        }
+        string formattedMessage = string.Format(message, args);
+        logger.Log(LogLevel.Information, formattedMessage);
+    }
+    public static void Debug(this BaseLogger logger, string message, params object[] args)
+    {
+        if (logger == null)
+        {
+            throw new ArgumentNullException(nameof(logger));
+        }
+        string formattedMessage = string.Format(message, args);
+        logger.Log(LogLevel.Debug, formattedMessage);
+    }
 }
