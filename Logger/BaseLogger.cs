1fb84c3fd35f17149c7b8960da5b2e5d78f158be
Yoko Parks <hellothereyoko@gmail.com>
2025-10-06 21:50:51 -0700

    Update BaseLogger.cs

--- Logger/BaseLogger.cs
+++ Logger/BaseLogger.cs
@@ -2,6 +2,9 @@
 
 public abstract class BaseLogger
 {
+    //req1 auto prop
+    
+    public string ClassName{ get; set} = string.Empty;
     public abstract void Log(LogLevel logLevel, string message);
 }
 
