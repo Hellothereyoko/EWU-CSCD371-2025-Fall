e31ce3fe09351f80a9e6bf49d2ce33bbca376cf0
Hellothereyoko <hellothereyoko@gmail.com>
2025-10-06 22:02:29 -0700

    Fixed Nico Nuking the Branch

--- Logger/LogFactory.cs
+++ Logger/LogFactory.cs
@@ -1,10 +1,19 @@
 ﻿namespace Logger;
 
+
 public class LogFactory
 {
-    public BaseLogger CreateLogger(string className)
-    {
+    private string? _filePath;
 
-        return null;
+    public void ConfigureFileLogger(string filePath)
+    {
+        _filePath = filePath;
+    }
+
+    public BaseLogger? CreateLogger(string className)
+    {
+        if (_filePath == null)
+            return null;
+        return new FileLogger(className, _filePath);
     }
 }
