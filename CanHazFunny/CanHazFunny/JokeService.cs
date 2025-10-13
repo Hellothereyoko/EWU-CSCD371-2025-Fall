47c55ded5fcb3a1156d139528e56714a056ada71
Ashley Giang <tgiang1@ewu.edu>
2025-10-12 22:13:42 -0700

    Update JokeService to retrieve jokes using JSON

--- CanHazFunny/CanHazFunny/JokeService.cs
+++ CanHazFunny/CanHazFunny/JokeService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http;
+using System.Text.Json;
 
 
 namespace CanHazFunny;
@@ -9,29 +10,34 @@ public class JokeService : IJokeService
 
     public string GetJoke()
     {
-        /*//Could use a bool to repeat this function until a joke w/o "Chuck Norris" is present
-
-        string joke = HttpClient.GetStringAsync("https://geek-jokes.sameerkumar.website/api").Result;
-
-
-        if (joke.Contains("Chuck Norris") || joke.Contains("Chuck") || joke.Contains("Norris"))
-        {
-
-            joke = "No Chuck Norris Jokes!";
-            return joke;
-        }
-        else
-            return joke;*/
 
         while (true)
         {
-            string joke = HttpClient.GetStringAsync("https://geek-jokes.sameerkumar.website/api").Result;
-            
-            if (!joke.Contains("Chuck Norris"))
+            try
             {
-                return joke;
+                //Request JSON instead of plain text
+                string json = HttpClient.GetStringAsync("https://geek-jokes.sameerkumar.website/api?format=json").Result;
+
+                //Parse JSON - turns the JSON text into a readable object
+                using var doc = JsonDocument.Parse(json);
+
+                //Gets the value of the key "joke" and turns that value into C# string
+                string joke = doc.RootElement.GetProperty("joke").GetString() ?? "No joke found";
+
+                //If jokes contain "Chuck Norris", loop again to get another joke
+                if (!joke.Contains("Chuck Norris"))
+                {
+                    return joke;
+                }
+                
             }
-            //If jokes contain "Chuck Norris", loop again to get another joke
+            catch
+            {
+                return "Error retrieving joke. Please try again later";
+            }
+
+
+
         }   
     }
 }
