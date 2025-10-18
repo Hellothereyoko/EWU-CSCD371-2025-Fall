8428bc8500a4e26a9eaf8bc17f6cf6019176a65d
Ashley Giang <tgiang1@ewu.edu>
2025-10-17 21:51:01 -0700

    Modify Employee

--- Logger/Employee.cs
+++ Logger/Employee.cs
@@ -1,13 +1,16 @@
 
 using System;
+using System.Security.Principal;
 
 namespace Logger;
 
 
 public record Employee : PersonEntity
-{
+{   
+    public string Role { get; init; }
     
-      public Employee(Guid id, FullName fullName, string email, string V) : base(id, fullName, email)
+      public Employee(Guid id, FullName fullName, string email, string role) : base(id, fullName, email)
     {
+        Role = role ?? throw new ArgumentNullException(nameof(role));
     }
 }
