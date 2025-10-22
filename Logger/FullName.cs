4d8bf5d6068931d3ecf36ee48d7012e1a4af4d06
Ashley Giang <tgiang1@ewu.edu>
2025-10-21 19:19:22 -0700

    Add comments on the full name record

--- Logger/FullName.cs
+++ Logger/FullName.cs
@@ -1,6 +1,12 @@
 namespace Logger;
+//FullName represents a person's full name with first, last, and optional middle name
+//It is defined as a record because it is data container
+//We want equality based on its values rather than the object's references
 
-    public record FullName
+//This type is immutable
+//Immutability ensures that once FullName instance is created, its data cannot
+//accidentally change, maintaining data consistency
+public record FullName
     {
         public string First { get; init; }
         public string Last { get; init; }
