718b521241f5dcb5137986401c9960f47dad5989
Ryan Hirte <rhirte@ewu.edu>
2025-11-01 10:38:06 -0700

    Program done I think
    Followed instructions for program class I think it's done.

---
+++ Calculate.Tests/ProgramTests.cs
@@ -0,0 +1,48 @@
+﻿namespace Calculate.Tests;
+
+[TestClass]
+public sealed class ProgramTests
+{
+    [TestMethod]
+    public void Constructor_InitializesWriteLineAndReadLine_Success()
+    {
+        // Arrange & Act
+        var program = new Program();
+        // Assert
+        Assert.IsNotNull(program.WriteLine);
+        Assert.IsNotNull(program.ReadLine);
+    }
+
+    [TestMethod]
+    public void WriteLine_WritesExpectedOutput_Success()
+    {
+        // Arrange
+        var output = string.Empty;
+        var program = new Program
+        {
+            // Override WriteLine to capture output
+            WriteLine = s => output = s
+        };
+        var expectedMessage = "Hello, World!";
+        // Act
+        program.WriteLine(expectedMessage);
+        // Assert
+        Assert.AreEqual<string>(expectedMessage, output);
+    }
+
+    [TestMethod]
+    public void ReadLine_ReturnsExpectedInput_Success()
+    {
+        // Arrange
+        var expectedInput = "Test Input";
+        var program = new Program
+        {
+            // Override ReadLine to return expected input
+            ReadLine = () => expectedInput
+        };
+        // Act
+        var actualInput = program.ReadLine();
+        // Assert
+        Assert.AreEqual<string>(expectedInput, actualInput);
+    }
+}
--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -2,6 +2,15 @@
 
 public class Program
 {
+
+    public Action<string> WriteLine {get; init;} = Console.WriteLine;
+
+    public Func<string?> ReadLine {get; init; } = Console.ReadLine;
+
+    public Program()
+    {
+    }
+
     public static void Main()
     {
     }
