cddd4994c82895a64949b301cc563ee2f4d81311
Hellothereyoko <hellothereyoko@gmail.com>
2025-11-18 22:39:57 -0800

    quick fix of sample data due to comp err

--- Assignment/Assignment/SampleData.cs
+++ Assignment/Assignment/SampleData.cs
@@ -1,12 +1,9 @@
-﻿
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
 namespace Assignment;
-
 
 public class SampleData : ISampleData
 {
@@ -17,52 +14,42 @@ public class SampleData : ISampleData
         _csvFilePath = csvFilePath;
     }
 
+    // 1. Implements CsvRows (skips header)
     public IEnumerable<string> CsvRows
     {
         get { return File.ReadLines(_csvFilePath).Skip(1); }
     }
 
-   public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows()
+    // 2. Returns unique and sorted states using CsvParser
+    public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows()
     {
         return CsvRows
-            // FIX: Call shared CsvParser method
-            .Select(CsvParser.ParseStateFromCsvRow)
+            .Select(CsvParser.ParseStateFromCsvRow) // Calls shared parser
             .Distinct()
             .OrderBy(state => state);
     }
 
-// ... (methods between)
-
-    public IEnumerable<IPerson> People
-    {
-        get
-        {
-            return CsvRows
-                // FIX: Call shared CsvParser method
-                .Select(CsvParser.ParsePersonFromCsvRow) 
-                .OrderBy(p => p.Address.State)
-                .ThenBy(p => p.Address.City)
-                .ThenBy(p => p.Address.Zip);
-        }
-    }
+    // 3. Aggregates unique and sorted states into a comma-separated string
     public string GetAggregateSortedListOfStatesUsingCsvRows()
     {
         var states = GetUniqueSortedListOfStatesGivenCsvRows().ToArray();
         return string.Join(", ", states);
     }
 
+    // 4. Returns Person objects sorted by State, City, and Zip using CsvParser
     public IEnumerable<IPerson> People
     {
         get
         {
             return CsvRows
-                .Select(ParsePersonFromCsvRow)
+                .Select(CsvParser.ParsePersonFromCsvRow) // Calls shared parser
                 .OrderBy(p => p.Address.State)
                 .ThenBy(p => p.Address.City)
                 .ThenBy(p => p.Address.Zip);
         }
     }
 
+    // 5. Filters the People collection by email
     public IEnumerable<(string FirstName, string LastName)> FilterByEmailAddress(Predicate<string> filter)
     {
         return People
@@ -70,6 +57,7 @@ public class SampleData : ISampleData
             .Select(p => (p.FirstName, p.LastName));
     }
 
+    // 6. Aggregates the states from the provided people collection
     public string GetAggregateListOfStatesGivenPeopleCollection(IEnumerable<IPerson> people)
     {
         var distinctStates = people
@@ -82,8 +70,4 @@ public class SampleData : ISampleData
 
         return distinctStates.Aggregate((current, next) => $"{current}, {next}");
     }
-
-    
-
-   
 }
