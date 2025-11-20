75ab17b1af36b699119c458d3acdbbb970424cd9
Hellothereyoko <hellothereyoko@gmail.com>
2025-11-20 14:48:46 -0800

    Started stubbing out Async Testing per Ryan's suggestion

---
+++ Assignment.Tests/SampleDataAsyncTests.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System; // <-- ADDED: Needed for StringComparison (keeps my compiler happy)           
+using Assignment;
+
+namespace Assignment.Tests;
+
+
+
+
+
+[TestClass] 
+public class SampleDataAsyncTests
+{
+    // Placeholder for future async tests
+    [TestMethod]
+    public void PlaceholderAsyncTest(){}
+
+
+
+}
