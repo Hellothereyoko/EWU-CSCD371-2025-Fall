57ee2866978f6b00a3b8bfd1a2287b676661629c
Hellothereyoko <hellothereyoko@gmail.com>
2025-12-03 23:36:33 -0800

    fix

--- Assignment.Tests/StringExtensions.cs
+++ Assignment.Tests/StringExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace IntelliTect.TestTools;
 
@@ -14,7 +13,7 @@ public static class StringExtensions
     /// <param name="s">The string to match</param>
     /// <param name="pattern">The pattern to match it against.</param>
     /// <returns></returns>
-    public static bool IsLikeRegEx(this string s, string pattern) =>
+    public static bool IsMatchRegex(this string s, string pattern) =>
         new Regex(pattern, RegexOptions.IgnoreCase).IsMatch(s);
 
     /// <summary>
