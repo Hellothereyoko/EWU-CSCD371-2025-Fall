09bdd90a3bcab0df08921ca1a783214d208b5d1a
Hellothereyoko <hellothereyoko@gmail.com>
2025-12-03 23:31:16 -0800

    fixes galore

--- Assignment/obj/Debug/net9.0/Assignment.AssemblyInfo.cs
+++ Assignment/obj/Debug/net9.0/Assignment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f08b2428e42c340833dbd13e9d12af07dc300295")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27aa22d2404dfeee2563026bf9e5fce4a892bb8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
